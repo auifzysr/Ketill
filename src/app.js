324fe94d6e0adbca4bc5f0f6ddbb7f8aaca25a8b
auifzysr <auifzysr@gmail.com>
2021-12-30 18:37:15 +0900

    Relocate

--- src/app.js
+++ src/app.js
@@ -3,14 +3,7 @@
 const { App } = require('@slack/bolt');
 const dotenv = require('dotenv');
 const path = require('path');
-
 const fs = require('fs');
-
-const tzListFile = 'tzlist.json'
-
-const getTZList = (tzListPath) => {
-	return JSON.parse(fs.readFileSync(tzListPath));
-};
 
 const result = dotenv.config({
 	path: path.resolve(process.cwd(), '.env')
@@ -19,15 +12,19 @@ if (result.error) {
 	throw result.error
 }
 
+const tzListFile = 'tzlist.json'
+const getTZList = (tzListPath) => {
+	return JSON.parse(fs.readFileSync(tzListPath));
+};
+const tzlist = getTZList(path.resolve(process.cwd(), tzListFile));
+const pattern = new RegExp(Object.keys(tzlist).join('|'), 'g');
+
 const app = new App({
 	token: process.env.SLACK_BOT_TOKEN,
 	signingSecret: process.env.SLACK_SIGNING_SECRET,
 	appToken: process.env.SLACK_APP_TOKEN,
 	port: process.env.PORT || 3000
 });
-
-const tzlist = getTZList(path.resolve(process.cwd(), tzListFile));
-const pattern = new RegExp(Object.keys(tzlist).join('|'), 'g');
 
 app.message(pattern, async ({ message, say }) => {
 	for(const tz of message.text.match(pattern)) {
