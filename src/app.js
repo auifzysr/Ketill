7aa19f79ef2286747e37385a1688a5736b2ed43b
auifzysr <auifzysr@gmail.com>
2021-12-30 16:28:59 +0900

    Respond to message

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const { App } = require('@slack/bolt');
+
+const app = new App({
+	token: process.env.SLACK_BOT_TOKEN,
+	signingSecret: process.env.SLACK_SIGNING_SECRET,
+	socketMode: true,
+	appToken: process.env.SLACK_APP_TOKEN,
+	port: process.env.PORT || 3000
+});
+
+app.message('hello', async ({ message, say }) => {
+	await say(`hey there <@${message.user}>!`);
+});
+
+(async () => {
+	await app.start();
+
+	console.log('it\'s running');
+})();
