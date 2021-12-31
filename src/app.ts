23184fe91a83535cca8b1a2267e522094e680e03
auifzysr <auifzysr@gmail.com>
2022-01-01 00:41:28 +0900

    Apply Prettier

--- src/app.ts
+++ src/app.ts
@@ -1,40 +1,41 @@
-'use strict';
+"use strict";
 
-import { App, BotMessageEvent, GenericMessageEvent } from '@slack/bolt';
+import { App, BotMessageEvent, GenericMessageEvent } from "@slack/bolt";
 
-import dotenv = require('dotenv');
-import path = require('path');
+import dotenv = require("dotenv");
+import path = require("path");
 
-import tz = require('./tz');
+import tz = require("./tz");
 
 const result = dotenv.config({
-	path: path.resolve(process.cwd(), '.env')
+  path: path.resolve(process.cwd(), ".env"),
 });
 if (result.error) {
-	throw result.error
+  throw result.error;
 }
 
 const app = new App({
-	token: process.env.SLACK_BOT_TOKEN,
-	signingSecret: process.env.SLACK_SIGNING_SECRET,
-	appToken: process.env.SLACK_APP_TOKEN,
-	port: Number(process.env.PORT) || 3000
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  appToken: process.env.SLACK_APP_TOKEN,
+  port: Number(process.env.PORT) || 3000,
 });
 
 const tzlist = tz.tzlist;
-const pattern = new RegExp(Object.keys(tz.tzlist).join('|'), 'g');
+const pattern = new RegExp(Object.keys(tz.tzlist).join("|"), "g");
 
 // temporary workaround
 // https://github.com/slackapi/bolt-js/issues/904
 app.message(pattern, async ({ message, say }) => {
-	for(const tz of (message as GenericMessageEvent).text?.match(pattern) || []) {
-		const hour_diff = tzlist[tz];
-		const response_body = `The time difference between *${tz}* and UTC is *${hour_diff} hour(s)*.`;
-		await say(response_body);
-	}
+  for (const tz of (message as GenericMessageEvent).text?.match(pattern) ||
+    []) {
+    const hour_diff = tzlist[tz];
+    const response_body = `The time difference between *${tz}* and UTC is *${hour_diff} hour(s)*.`;
+    await say(response_body);
+  }
 });
 
 (async () => {
-	await app.start();
-	console.log('it\'s running');
+  await app.start();
+  console.log("it's running");
 })();
--- src/tz.ts
+++ src/tz.ts
@@ -1,10 +1,10 @@
-'use strict';
+"use strict";
 
-import path = require('path');
-import fs = require('fs');
+import path = require("path");
+import fs = require("fs");
 
-const tzListFile = 'tzlist.json'
+const tzListFile = "tzlist.json";
 
 export const tzlist = ((tzListPath) => {
-	return JSON.parse(fs.readFileSync(tzListPath).toString());
+  return JSON.parse(fs.readFileSync(tzListPath).toString());
 })(path.resolve(process.cwd(), tzListFile));
